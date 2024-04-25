afc3eeaaae48aa40d35942bfb561a462fa51e006
lakakauser <168128416+lakakauser@users.noreply.github.com>
2024-04-25 16:49:35 -0300

    CustomChar.lua

---
+++ CustomChar/main/CustomChar.lua
@@ -0,0 +1,23 @@
+local Tabela = {
+	PlayerPermission = {
+	},
+	
+	CharacterTier = {
+		["Enrico"] = 					{Tier = 1,Permission = {2582436510}},
+		["DIO"]	=					{Locked = {2582436510}},
+		["Madara"]	=				{Locked = {2582436510}},
+		["Koku"]	=				{Locked = {2582436510}},
+		["Yori"]	=				{Permission = {2582436510}},
+		["Meliodas"]	=				{Permission = {2582436510}},
+		["Goku"]	=				{Locked = {2582436510}},
+		["TOP"]	=					{Locked = {2582436510,128677359}},
+		["Giorno"] = 					{Tier = 1050,Permission = {2582436510}},
+		["Hakaishin"] = 				{Locked = {2582436510}},
+		["Boss"] = 					{Locked = {2582436510}},
+		["JP6"] = 					{Tier = 1,Permission = {2582436510}},
+		["Gojo"] = 					{Tier = 1,Permission = {2582436510}}, -- perm temp
+	}
+
+}
+
+return Tabela
