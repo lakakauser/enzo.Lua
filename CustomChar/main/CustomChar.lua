97425c08fb83641d5d30ff9036c04b4fc9696ae3
lakakauser <168128416+lakakauser@users.noreply.github.com>
2024-10-06 20:32:42 -0300

    CustomChar.lua

--- CustomChar/main/CustomChar.lua
+++ CustomChar/main/CustomChar.lua
@@ -1,22 +1,30 @@
 local Tabela = {
 	PlayerPermission = {
-		["oibr1sa"] = 			{Tier = 2000},
+		["Oibr1sa"] = 			{Tier = 2000},
+		["DanielWINCHEST3R"] = 			{Tier = 2000},
+		["felipeofc98"] = 				{Tier = 2000},
+		["AntarisOpp"] = 				{Tier = 2000},
+		["TROPA_TUDO"] = 				{Tier = 2000},
+		["P3U_EB"] = 			{Tier = 2000},
+		["bruno_jales"] = 			{Tier = 2000},
+		["Toniket457"] = 			{Tier = 2000},
+		["dudu123123swq"] = 				{Tier = 2000},
 	},
 	
 	CharacterTier = {
-		["Enrico"] = 					{Tier = 1050,Permission = {2582436510}},
+		["Enrico"] = 					{Tier = 1,Permission = {2582436510,4035850120,1706462376}},
 		["DIO"]	=					{Locked = {2582436510}},
 		["Madara"]	=				{Locked = {2582436510}},
-		["Koku"]	=				{Locked = {2582436510}},
-		["Yori"]	=				{Permission = {2582436510}},
-		["Meliodas"]	=				{Permission = {2582436510}},
+		["Koku"]	=				{Locked = {2582436510,327518760}},
+		["Yori"]	=				{Permission = {2582436510,3108673472,1706462376,2987691941}},
+		["Meliodas"]	=				{Permission = {2582436510,629259254,785068132,3108673472}},
 		["Goku"]	=				{Locked = {2582436510}},
-		["TOP"]	=					{Locked = {2582436510,}},
-		["Giorno"] = 					{Tier = 1050,Permission = {2582436510}},
-		["Hakaishin"] = 				{Locked = {2582436510}},
-		["Boss"] = 					{Locked = {2582436510}},
-		["JP6"] = 					{Tier = 1050,Permission = {2582436510}},
-		["Gojo"] = 					{Locked = {2582436510,}},
+		["TOP"]	=					{Locked = {2582436510}},
+		["Giorno"] = 					{Tier = 1050,Permission = {2582436510,785068132,3108673472,834610807}},
+		["Hakaishin"] = 				{Locked = {36651812,2582436510,785068132,629259254}},
+		["Boss"] = 					{Locked = {785068132,2582436510,4035850120}},
+		["JP6"] = 					{Tier = 1,Permission = {2582436510,2908376678,1706565385,3108673472,3162059849}},
+		["Gojo"] = 					{Tier = 1,Permission = {2582436510,2963030044}}, -- perm temp
 	}
 
 }
